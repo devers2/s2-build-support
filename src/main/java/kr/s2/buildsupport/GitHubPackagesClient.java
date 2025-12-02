0707d5bd4343777ad44cb49658cd9a6fe315ab3a
s2 <eseungsu@placelink.shop>
2025-12-02 10:36:58 +0900

    패키지 경로 변경

--- src/main/java/kr/s2/buildsupport/GitHubPackagesClient.java
+++ src/main/java/kr/s2/buildsupport/GitHubPackagesClient.java
@@ -89,7 +89,7 @@ public class GitHubPackagesClient {
      */
     public static boolean isRepoPrivate(String repoBaseUrl, String githubToken) {
         // REPO_BASE_URL에서 owner/repo 추출
-        // 예: "https://maven.pkg.github.com/Placelink-HUB/libs-repo" -> "Placelink-HUB/libs-repo"
+        // 예: "https://maven.pkg.github.com/Placelink-HUB/packages" -> "Placelink-HUB/packages"
         Pattern pattern = Pattern.compile("github\\.com/([^/]+)/([^/]+)");
         Matcher matcher = pattern.matcher(repoBaseUrl);
 
