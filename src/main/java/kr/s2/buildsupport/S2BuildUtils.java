9d2462473dd186b57e5eb66c6cd2471e69ec00e4
s2 <eseungsu.dev@gmail.com>
2025-12-21 08:33:21 +0900

    Javadoc 인코딩 설정 추가

--- src/main/java/kr/s2/buildsupport/S2BuildUtils.java
+++ src/main/java/kr/s2/buildsupport/S2BuildUtils.java
@@ -523,6 +523,9 @@ public class S2BuildUtils {
             options.setDocEncoding("UTF-8");
             options.setCharSet("UTF-8");
             options.setJFlags(java.util.Arrays.asList("-Dfile.encoding=UTF-8"));
+            options.addStringOption("encoding", "UTF-8");
+            options.addStringOption("docencoding", "UTF-8");
+            options.addStringOption("charset", "UTF-8");
 
             // 모든 경고 및 오류 검사 비활성화
             options.addStringOption("Xdoclint:none", "-quiet");
